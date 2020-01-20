a18736bc5dbef285f238f67de8b50a1c57c6c887
Austin Johnson <technicallyaustin@gmail.com>
2020-01-20 21:05:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,7 +84,11 @@ end
 
 
 def won?
-  WIN_COMBINATIONS. # 1st 
+  WIN_COMBINATIONS.select { |win_array|
+  win_array[0] == "X" && win_array[1] == "X" && win_array[2] == "X" ||
+  win_array[0] == "O" && win_array[1] == "O" && win_array[2] == "O"}
+end
+  
    
   
  
