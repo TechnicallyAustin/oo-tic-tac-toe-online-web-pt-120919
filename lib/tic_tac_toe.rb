9670501e0ea0e281e6aebaed9667fb70736e2226
Austin Johnson <technicallyaustin@gmail.com>
2020-01-21 00:33:43 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,6 @@
 require 'pry'
 class TicTacToe
+  
   WIN_COMBINATIONS = [
   #Row WIN_COMBINATIONS
   [0,1,2],
@@ -39,72 +40,115 @@ end
 
 def position_taken?(index)
  !(@board[index].nil? || @board[index] == " ")
-   
-
   #binding.pry 
 
 end
 
 def valid_move?(index)
-  if position_taken?(index) == false && index < 9 
-    true 
-  else 
-    false 
-  end 
+  if !position_taken?(index) && index.between?(0,8)
+true
+end
   
 end
 
-def turn(moves, player = "X")
-  puts "Pick an area 1 - 9"
-  selected = input_to_index(moves)
-  if position_taken?(selected) == false 
-    @board[selected] = player
-    @board
-  else
-    while position_taken?(selected) == true 
-    puts "Area is currently taken."
-    @board 
-    puts "Pick an area 1 - 9"
-    selected
-  end
-end
-end
 
 def turn_count
-  @x_arr = @board.select { |i| i == "X"}
-  @o_arr = @board.select { |i| i == "O"}
-  new_arr = x_arr + o_arr
-  new_arr.length 
+  board_arr = @board.select { |i| i == "X" || i == "O" }
+  board_arr.length
 end
 
 def current_player
-  if @x_arr > @o_arr
-    puts "X's turn"
-  else puts "O's turn"
+  if turn_count % 2 == 0 
+    player = "X"
+    player 
+  else
+    player = "O"
+    player 
 end
-    #use a boolean search enumerator for 
-    #does the return value of turn_count have more X's than O's 
-    #if true its the turn of whoever has less turns taken .
-    
 end
+
+def turn
+  puts "Pick an area 1 - 9"
+  input = gets.chomp
+  board_pos = input_to_index(input)
+  if valid_move?(board_pos)
+  player_symbol = current_player
+  move(board_pos, player_symbol)
+  display_board
+else
+  turn
+end
+
+end
+  
+
 
 def won?
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return win_combo
+      
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
+      
+    end
+      false
+  end
 end
+   
+  
+ 
 
 def full?
-end
+ if @board.include?(" ")
+   false
+ else
+   true
+   
+ end
+ end
 
 def draw?
+  if full? && !won?
+      true
+    elsif !full? && !won?
+    false
+  else won?
+    false
+  end
+    
 end
 
 def over?
+  if full? || won? || draw?
+    true
+  end
 end
 
-def winner 
+def winner
+  WIN_COMBINATIONS.each do |combo|
+    if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+      return "X"
+    elsif 
+    @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+    return "O"
+end
+end
+nil
 end
 
-def play 
+def play
+  until over? == true
+  if !draw?
+    turn
+  end
 end
 
-    
+  if won?
+  puts "Congratulations #{winner}!"
+elsif draw?
+puts  "Cat's Game!"
+end
+end
+
 end
